c3ea717035ef54dd1fa509521525aa47e35e24fc
afayze <99722892+afayze@users.noreply.github.com>
2022-02-22 11:43:29 +0100

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -4,12 +4,13 @@ Created on Tue Feb 22 10:14:27 2022
 
 @author: Andres.Malon
 """
+import sys
 class Stack():
     def __init__(self):
         self.items=[]
     def push(self,item):
-        self.items.append()
-    def pop (self,item):
+        self.items.append(item)
+    def pop (self):
         if self.isEmpty():
             raise RuntimeError("Attempt to pop an empty stack")
         topIdx = len(self.items)-1
@@ -21,11 +22,27 @@ class Stack():
                 raise RuntimeError("Attempt to pop an empty stack")
         topIdx = len(self.items)-1
         item = self.items[topIdx]
-        return self.items[topIdx]
+        return item
     
     def isEmpty (self):
         return len(self.items) == 0
-
+    
+def Check_balance(a):
+    s=Stack()
+    check={')':'(', ']':'[', '}':'{'}
+    for i in range(0,len(a)):
+        if a[i]=='(' or a[i]=='[' or a[i]=='{':
+            s.push(a[i])
+        elif a[i]==')' or a[i]=='}' or a[i]==']':
+            if check[a[i]]==s.pop():
+                print()
+            else:
+                print('Not balanced')
+                sys.exit(0)
+    if  s.isEmpty():
+        print('Balanced')
+    else:
+        print('Not balanced')
 
 
 
