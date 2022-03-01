55d11b04fbad55db177dadd391c44955c23765a5
afayze <99722892+afayze@users.noreply.github.com>
2022-03-01 11:26:17 +0100

    Update TryPalindromeWord.py

--- TryPalindromeWord.py
+++ TryPalindromeWord.py
@@ -1,17 +1,32 @@
 #TryPalindromeWord
-import TryDeque.py as TD
-       
+from TryDeque import Deque
+from Stack import Stack 
 
 word=input("Introduce a word to be checked: ")
+'''def Check_balance(a):
+    s=Stack()
+    check={')':'(', ']':'[', '}':'{'}
+    for i in range(0,len(a)):
+        if a[i]=='(' or a[i]=='[' or a[i]=='{':
+            s.push(a[i])
+        elif a[i]==')' or a[i]=='}' or a[i]==']':
+            if check[a[i]]==s.pop():
+                print()
+            else:
+                print('Not balanced')
+                sys.exit(0)
+    if  s.isEmpty():
+        return True
+    else:
+        return False'''
 
 def check_Palindrome(word):
     chars=list(word)
-    D=TD.Deque()
+    D=Deque()
     for letter in chars:
         D.add_front(letter.upper())
     
     check=True
-    print()
     while D.size()>1 and check:
         if D.items[0]!=D.items[-1]:
             print('The word isn´t palindromic')
